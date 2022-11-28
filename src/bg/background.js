39d7166176571b569c0ad554f096b1b65ce13180
Vladimir <vladimir.mikhailov@toptal.com>
2022-11-28 22:01:58 +0300

    Update manifest to v3

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,6 +1,8 @@
 //example of using a message handler from the inject scripts
+chrome.action.disable();
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    chrome.pageAction.show(sender.tab.id);
+    chrome.action.enable(sender.tab.id);
     sendResponse();
   });
